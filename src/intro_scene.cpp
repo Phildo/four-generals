37ebf38e2995db66a05bf904a967438e584fc048
Phildo <phildo211@gmail.com>
2014-09-29 16:36:50 -0500

    sample box drawing

--- src/intro_scene.cpp
+++ src/intro_scene.cpp
@@ -24,7 +24,24 @@ void IntroScene::tick()
 
 void IntroScene::draw()
 {
-  graphics->drawAt(Sprite::tl_border(),0,0);
+  graphics->drawAt(Sprite::tl_border(),20,20);
+  graphics->drawAt(Sprite::t_border(), 30,20);
+  graphics->drawAt(Sprite::t_border(), 40,20);
+  graphics->drawAt(Sprite::t_border(), 50,20);
+  graphics->drawAt(Sprite::t_border(), 60,20);
+  graphics->drawAt(Sprite::t_border(), 70,20);
+  graphics->drawAt(Sprite::tr_border(),80,20);
+  graphics->drawAt(Sprite::r_border(), 80,30);
+  graphics->drawAt(Sprite::r_border(), 80,40);
+  graphics->drawAt(Sprite::br_border(),80,50);
+  graphics->drawAt(Sprite::b_border(), 70,50);
+  graphics->drawAt(Sprite::b_border(), 60,50);
+  graphics->drawAt(Sprite::b_border(), 50,50);
+  graphics->drawAt(Sprite::b_border(), 40,50);
+  graphics->drawAt(Sprite::b_border(), 30,50);
+  graphics->drawAt(Sprite::bl_border(),20,50);
+  graphics->drawAt(Sprite::l_border(), 20,40);
+  graphics->drawAt(Sprite::l_border(), 20,30);
 }
 
 Scene::~Scene()
