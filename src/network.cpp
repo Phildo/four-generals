101b31680b4f459f6aeb27113fea4047a199329b
Phildo <phildo211@gmail.com>
2014-10-04 14:35:44 -0500

    added commented out c networking

--- src/network.cpp
+++ src/network.cpp
@@ -3,48 +3,80 @@
 #include "defines.h"
 #include "logger.h"
 
-#ifdef FG_ANDROID
-#include "android_network.cpp"
-#endif
-#ifdef FG_IOS
-#include "ios_network.cpp"
-#endif
-#ifdef FG_PC
-#include "pc_network.cpp"
-#endif
-
 Network::Network()
 {
-  #ifdef FG_ANDROID
-    AndroidNetwork::construct();
-  #endif
 }
 
 Network::~Network()
 {
-  #ifdef FG_ANDROID
-    AndroidNetwork::destruct();
-  #endif
 }
 
 void Network::connectAsServer()
 {
-  #ifdef FG_ANDROID
-    AndroidNetwork::connectAsServer();
-  #endif
+  /*
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd < 0) fg_log("Nope");
+
+  bzero(&serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+
+  if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+    fg_log("Nope");
+  listen(sockfd,5);
+
+  clilen = sizeof(cli_addr);
+  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+  if(newsockfd < 0) fg_log("Nope");
+
+  bzero(buffer, 256);
+  n = read(newsockfd, buffer, 255);
+  if(n < 0) fg_log("Nope");
+
+  printf("got dat message: %s\n",buffer);
+  n = write(newsockfd,"I gotchu",8);
+  if(n < 0) fg_log("Nope");
+
+  close(newsockfd);
+  close(sockfd);
+  */
 }
 
 void Network::connectAsClient()
 {
-  #ifdef FG_ANDROID
-    AndroidNetwork::connectAsClient();
-  #endif
+  /*
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd < 0) fg_log("Nope");
+
+  server = gethostbyname("blah.com");//should take raw ip
+  if(server == NULL) fg_log("Nope");
+
+  bzero((char *)&serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+
+  bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+  serv_addr.sin_port = htons(portno);
+
+  if(connect(sockfd,(struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    fg_log("Nope");
+
+  printf("Massage?: ");
+  bzero(buffer,256);
+  fgets(buffer,255,stdin);
+  n = write(sockfd, buffer, strlen(buffer));
+  if(n < 0) fg_log("Nope");
+
+  bzero(buffer, 256);
+  n = read(sockfd,buffer,255);
+  if(n < 0) fg_log("Nope");
+
+  printf("%s\n",buffer);
+  close(sockfd);
+  */
 }
 
 void Network::broadcast(char *c, int l)
 {
-  #ifdef FG_ANDROID
-    AndroidNetwork::broadcast(c,l);
-  #endif
 }
 
--- src/network.h
+++ src/network.h
@@ -4,12 +4,37 @@
 #define MAX_PORT_LENGTH 20
 #define MAX_IP_LENGTH 256 //really only need 12, but might as well leave it for urls?
 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+*/
+
 class Network
 {
   private:
     bool host_priv;
     char port[MAX_PORT_LENGTH];
     char ip[MAX_IP_LENGTH];
+
+    /*
+    int sockfd;
+    int newsockfd;
+    static const int portno = 8080;
+    int n;
+
+    socklen_t clilen;
+    char buffer[256];
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in cli_addr;
+    struct hostent *server;
+    */
+
   public:
     Network();
     ~Network();
