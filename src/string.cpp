9340d4485f9d35e1b6567dca14e961014552e90e
Phildo <phildo211@gmail.com>
2014-10-13 11:31:11 -0500

    theoretical strings

--- src/string.cpp
+++ src/string.cpp
@@ -1,12 +1,28 @@
 #include "string.h"
 
-String::String()
+String::String(const char* s, int l)
 {
+  length = l;
+  if(l <= FG_STRING_OBJECT_LENGTH) handle = &object[0];
+  else                             handle = new char[l];
 
+  for(int i = 0; i < l; i++)
+    handle[i] = s[i];
 }
 
 String::~String()
 {
+  if(handle != &object[0])
+  delete[] handle;
+}
 
+int String::len()
+{
+  return length;
+}
+
+char *String::ptr()
+{
+  return handle;
 }
 
--- src/string.h
+++ src/string.h
@@ -1,12 +1,18 @@
 #ifndef _FG_STRING_H_
 #define _FG_STRING_H_
 
+#define FG_STRING_OBJECT_LENGTH 32
 class String
 {
   private:
+    char object[FG_STRING_OBJECT_LENGTH];
+    char *handle;
+    int length;
   public:
-    String();
+    String(const char* s, int l);
     ~String();
+    int len();
+    char *ptr();
 };
 
 #endif
