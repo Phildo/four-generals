b10de945a05b4d2cdcb6a29b993ed7673965ef85
Phildo <phildo211@gmail.com>
2014-10-30 20:38:12 -0500

    correct orientation in play screen

--- pc-project/run4.sh
+++ pc-project/run4.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-amake run > out/1.out &
-amake run > out/2.out &
-amake run > out/3.out &
-amake run > out/4.out &
+amake build
+../bin/pc.out > out/1.out &
+../bin/pc.out > out/2.out &
+../bin/pc.out > out/3.out &
+../bin/pc.out > out/4.out &
 
--- src/play_scene.cpp
+++ src/play_scene.cpp
@@ -25,7 +25,7 @@ PlayScene::PlayScene(Graphics *g, Network::Client *&c, ServerModel *&sm, ClientM
 
   backButton = UI::Button(10,10,20,20);
 
-#define pos(x) (10+((ww-20)/7)*x)+((ww-20)/7)/2-10
+#define pos(x) (30+((ww-60)/7)*x)+((ww-60)/7)/2-10
 
   sunLabel = UI::Label(pos(0), 10, 20, "Su");
   monLabel = UI::Label(pos(1), 10, 20, "Mo");
@@ -34,11 +34,6 @@ PlayScene::PlayScene(Graphics *g, Network::Client *&c, ServerModel *&sm, ClientM
   thuLabel = UI::Label(pos(4), 10, 20, "Th");
   friLabel = UI::Label(pos(5), 10, 20, "Fr");
   satLabel = UI::Label(pos(6), 10, 20, "Sa");
-
-  nLabel = UI::Label(ww/2-10,      10, 20, "N");
-  sLabel = UI::Label(ww/2-10, wh  -30, 20, "S");
-  wLabel = UI::Label(     10, wh/2-10, 20, "W");
-  eLabel = UI::Label(ww  -30, wh/2-10, 20, "E");
 
   attackLabel = UI::Label(       20, wh-30, 20, "attack"); attackButton = UI::Button(       20, wh-30, 100, 20);
   defendLabel = UI::Label(ww-100-20, wh-30, 20, "defend"); defendButton = UI::Button(ww-100-20, wh-30, 100, 20);
@@ -49,6 +44,21 @@ void PlayScene::enter()
   client = *client_ptr;
   s_model = *s_model_ptr;
   c_model = *c_model_ptr;
+
+  //need to wait until c_model is obtained
+  int ww = graphics->winWidth();
+  int wh = graphics->winHeight();
+
+  char cardinals[] = "N\0E\0S\0W\0";
+  int card = 0;
+  if(c_model->cardGeneral('n')->connection == client->connection) card = 0;
+  if(c_model->cardGeneral('e')->connection == client->connection) card = 1;
+  if(c_model->cardGeneral('s')->connection == client->connection) card = 2;
+  if(c_model->cardGeneral('w')->connection == client->connection) card = 3;
+  sLabel = UI::Label(ww/2-10, wh  -80, 20, &cardinals[((card+0)%4)*2]); //S //you
+  wLabel = UI::Label(     10, wh/2-10, 20, &cardinals[((card+1)%4)*2]); //W
+  nLabel = UI::Label(ww/2-10,      60, 20, &cardinals[((card+2)%4)*2]); //N
+  eLabel = UI::Label(ww  -30, wh/2-10, 20, &cardinals[((card+3)%4)*2]); //E
 }
 
 void PlayScene::touch(In &in)
