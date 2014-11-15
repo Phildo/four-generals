1acf6f7fc03693af0e72c4aa6cd02dd7352c31a3
Phildo <phildo211@gmail.com>
2014-11-15 17:54:11 -0600

    screen size API abstracts sizing

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -1,24 +1,30 @@
 #include "graphics.h"
 #include "defines.h"
 
-#ifndef FG_HALF_SIZE
 #define FG_SAFE_WIDTH 1184
 #define FG_SAFE_HEIGHT 720
-#else
-#define FG_SAFE_WIDTH 592
-#define FG_SAFE_HEIGHT 360
-#endif
 
 Graphics::Graphics()
 {
   #ifdef FG_ANDROID
   window = SDL_CreateWindow(NULL, 0, 0, 0, 0, 0);
   #elif defined FG_PC
-  window = SDL_CreateWindow(NULL, 0, 0, FG_SAFE_WIDTH, FG_SAFE_HEIGHT, 0);
+    #ifdef FG_HALF_SIZE
+    window = SDL_CreateWindow(NULL, 0, 0, FG_SAFE_WIDTH/2, FG_SAFE_HEIGHT/2, 0);
+    #else
+    window = SDL_CreateWindow(NULL, 0, 0, FG_SAFE_WIDTH, FG_SAFE_HEIGHT, 0);
+    #endif
   #endif
 
-  offsetX = (this->winWidth()-FG_SAFE_WIDTH)/2;
-  offsetY = (this->winHeight()-FG_SAFE_HEIGHT)/2;
+  int w; SDL_GetWindowSize(window, &w, NULL);
+  int h; SDL_GetWindowSize(window, NULL, &h);
+  #ifdef FG_HALF_SIZE
+  offsetX = (w-(FG_SAFE_WIDTH/2))/2;
+  offsetY = (h-(FG_SAFE_HEIGHT/2))/2;
+  #else
+  offsetX = (w-FG_SAFE_WIDTH)/2;
+  offsetY = (h-FG_SAFE_HEIGHT)/2;
+  #endif
 
   renderer = SDL_CreateRenderer(window, -1, 0);
 
@@ -47,20 +53,34 @@ Graphics::~Graphics()
 void Graphics::draw(const SDL_Rect& src, const SDL_Rect& dest)
 {
   SDL_Rect offsetDest;
+  #ifdef FG_HALF_SIZE
+  offsetDest.x = (dest.x/2)+offsetX;
+  offsetDest.y = (dest.y/2)+offsetY;
+  offsetDest.w = (dest.w/2);
+  offsetDest.h = (dest.h/2);
+  #else
   offsetDest.x = dest.x+offsetX;
   offsetDest.y = dest.y+offsetY;
   offsetDest.w = dest.w;
   offsetDest.h = dest.h;
+  #endif
   SDL_RenderCopy(renderer, tex, &src, &offsetDest);
 }
 
 void Graphics::drawAt(const SDL_Rect& src, int x, int y)
 {
   SDL_Rect offsetDest;
+  #ifdef FG_HALF_SIZE
+  offsetDest.x = (x/2)+offsetX;
+  offsetDest.y = (y/2)+offsetY;
+  offsetDest.w = (src.w/2);
+  offsetDest.h = (src.h/2);
+  #else
   offsetDest.x = x+offsetX;
   offsetDest.y = y+offsetY;
   offsetDest.w = src.w;
   offsetDest.h = src.h;
+  #endif
   SDL_RenderCopy(renderer, tex, &src, &offsetDest);
 }
 
@@ -74,8 +94,8 @@ void Graphics::flip()
   SDL_RenderPresent(renderer);
 }
 
-int Graphics::winWidth()  { int w; SDL_GetWindowSize(window, &w, NULL); return w; }
-int Graphics::winHeight() { int h; SDL_GetWindowSize(window, NULL, &h); return h; }
+int Graphics::winWidth()  { return FG_SAFE_WIDTH; }
+int Graphics::winHeight() { return FG_SAFE_HEIGHT; }
 int Graphics::texWidth()  { int w; SDL_QueryTexture(tex, NULL, NULL, &w, NULL); return w; }
 int Graphics::texHeight() { int h; SDL_QueryTexture(tex, NULL, NULL, NULL, &h); return h; }
 
--- src/input.cpp
+++ src/input.cpp
@@ -20,15 +20,25 @@ bool Input::poll(In &in, bool &put, bool &quit)
     else if(event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT)
     {
       put = true;
+      #ifdef FG_HALF_SIZE
+      in.x = event.button.x*2;
+      in.y = event.button.y*2;
+      #else
       in.x = event.button.x;
       in.y = event.button.y;
+      #endif
     }
     #elif defined FG_ANDROID
     else if(event.type == SDL_FINGERDOWN)
     {
       put = true;
+      #ifdef FG_HALF_SIZE
+      in.x = event.tfinger.x*winWidth*2;
+      in.y = event.tfinger.y*winHeight*2;
+      #else
       in.x = event.tfinger.x*winWidth;
       in.y = event.tfinger.y*winHeight;
+      #endif
     }
     #endif
     return true;
