b7bf63c2c6ab2cedba21a9899481d03f8daa1d0c
Phildo <phildo211@gmail.com>
2015-01-27 22:06:48 -0600

    specify drawing

--- src/specify_turn_picker.cpp
+++ src/specify_turn_picker.cpp
@@ -4,34 +4,79 @@
 
 #define SPECIFIER_HOW_IMPL \
   how = UI::Label("How will you sabotage?",r.x+10,r.y+10,35); \
-  how_block  = UI::ImageButton(Sprite::sblock,  space(r.w,0,100,3,0), r.y+50, 100, 100); \
-  how_read   = UI::ImageButton(Sprite::sread,   space(r.w,0,100,3,1), r.y+50, 100, 100); \
-  how_switch = UI::ImageButton(Sprite::sswitch, space(r.w,0,100,3,2), r.y+50, 100, 100);
+  how_block  = UI::ImageButton(Sprite::sblock,  r.x+space(r.w,0,100,3,0), r.y+50, 100, 100); \
+  how_read   = UI::ImageButton(Sprite::sread,   r.x+space(r.w,0,100,3,1), r.y+50, 100, 100); \
+  how_switch = UI::ImageButton(Sprite::sswitch, r.x+space(r.w,0,100,3,2), r.y+50, 100, 100);
 
 #define SPECIFIER_WHICH_IMPL \
   which = UI::Label("Which data will you switch?",r.x+10,r.y+10,35); \
-  which_who  = UI::ImageButton(Sprite::sblock, space(r.w,0,100,2,0), r.y+50, 100, 100); \
-  which_when = UI::ImageButton(Sprite::sun,    space(r.w,0,100,2,1), r.y+50, 100, 100);
+  which_who  = UI::ImageButton(Sprite::sblock, r.x+space(r.w,0,100,2,0), r.y+50, 100, 100); \
+  which_when = UI::ImageButton(Sprite::sun,    r.x+space(r.w,0,100,2,1), r.y+50, 100, 100);
 
 #define SPECIFIER_WHO_IMPL \
   who = UI::Label("Who?",r.x+10,r.y+10,35); \
-  who_cw  = UI::ImageButton(Sprite::sblock, space(r.w,0,100,2,0), r.y+50, 100, 100); \
-  who_ccw = UI::ImageButton(Sprite::sblock, space(r.w,0,100,2,1), r.y+50, 100, 100);
+  who_cw  = UI::ImageButton(Sprite::sblock, r.x+space(r.w,0,100,2,0), r.y+50, 100, 100); \
+  who_ccw = UI::ImageButton(Sprite::sblock, r.x+space(r.w,0,100,2,1), r.y+50, 100, 100);
 
 #define SPECIFIER_WHEN_IMPL \
   when = UI::Label("When?",r.x+10,r.y+10,35); \
-  when_su = UI::ImageButton(Sprite::sun, space(r.w,0,100,7,0), r.y+50, 100, 100); \
-  when_mo = UI::ImageButton(Sprite::sun, space(r.w,0,100,7,1), r.y+50, 100, 100); \
-  when_tu = UI::ImageButton(Sprite::sun, space(r.w,0,100,7,2), r.y+50, 100, 100); \
-  when_we = UI::ImageButton(Sprite::sun, space(r.w,0,100,7,3), r.y+50, 100, 100); \
-  when_th = UI::ImageButton(Sprite::sun, space(r.w,0,100,7,4), r.y+50, 100, 100); \
-  when_fr = UI::ImageButton(Sprite::sun, space(r.w,0,100,7,5), r.y+50, 100, 100); \
-  when_sa = UI::ImageButton(Sprite::sun, space(r.w,0,100,7,6), r.y+50, 100, 100);
+  when_su = UI::ImageButton(Sprite::sun, r.x+space(r.w,0,100,7,0), r.y+50, 100, 100); \
+  when_mo = UI::ImageButton(Sprite::sun, r.x+space(r.w,0,100,7,1), r.y+50, 100, 100); \
+  when_tu = UI::ImageButton(Sprite::sun, r.x+space(r.w,0,100,7,2), r.y+50, 100, 100); \
+  when_we = UI::ImageButton(Sprite::sun, r.x+space(r.w,0,100,7,3), r.y+50, 100, 100); \
+  when_th = UI::ImageButton(Sprite::sun, r.x+space(r.w,0,100,7,4), r.y+50, 100, 100); \
+  when_fr = UI::ImageButton(Sprite::sun, r.x+space(r.w,0,100,7,5), r.y+50, 100, 100); \
+  when_sa = UI::ImageButton(Sprite::sun, r.x+space(r.w,0,100,7,6), r.y+50, 100, 100);
 
 #define SPECIFIER_ROUTE_IMPL \
   route = UI::Label("Which route should the messenger take?",r.x+10,r.y+10,35); \
-  route_cw  = UI::ImageButton(Sprite::sblock, space(r.w,0,100,2,0), r.y+50, 100, 100); \
-  route_ccw = UI::ImageButton(Sprite::sblock, space(r.w,0,100,2,1), r.y+50, 100, 100);
+  route_cw  = UI::ImageButton(Sprite::sblock, r.x+space(r.w,0,100,2,0), r.y+50, 100, 100); \
+  route_ccw = UI::ImageButton(Sprite::sblock, r.x+space(r.w,0,100,2,1), r.y+50, 100, 100);
+
+
+
+#define SPECIFIER_DRAW_HOW_IMPL \
+{ \
+  how.draw(g); \
+  how_block.draw(g); \
+  how_read.draw(g); \
+  how_switch.draw(g); \
+}
+
+#define SPECIFIER_DRAW_WHICH_IMPL \
+{ \
+  which.draw(g); \
+  which_who.draw(g); \
+  which_when.draw(g); \
+}
+
+#define SPECIFIER_DRAW_WHO_IMPL \
+{ \
+  who.draw(g); \
+  who_cw.draw(g); \
+  who_ccw.draw(g); \
+}
+
+#define SPECIFIER_DRAW_WHEN_IMPL \
+{ \
+  when.draw(g); \
+  when_su.draw(g); \
+  when_mo.draw(g); \
+  when_tu.draw(g); \
+  when_we.draw(g); \
+  when_th.draw(g); \
+  when_fr.draw(g); \
+  when_sa.draw(g); \
+}
+
+#define SPECIFIER_DRAW_ROUTE_IMPL \
+{ \
+  route.draw(g); \
+  route_cw.draw(g); \
+  route_ccw.draw(g); \
+}
+
+
 
 void AttackSpecifier::init(SDL_Rect r)
 {
@@ -49,7 +94,9 @@ void AttackSpecifier::tick()
 }
 void AttackSpecifier::draw(Graphics *g)
 {
-
+  rect.draw(g);
+  if(action->who == '0')
+    SPECIFIER_DRAW_WHO_IMPL
 }
 
 void DefendSpecifier::init(SDL_Rect r)
@@ -89,16 +136,21 @@ void MessageSpecifier::tick()
 }
 void MessageSpecifier::draw(Graphics *g)
 {
-
+  if(action->who == '0')
+    SPECIFIER_DRAW_WHO_IMPL
+  else if(action->when == '0')
+    SPECIFIER_DRAW_WHEN_IMPL
+  else if(action->route == '0')
+    SPECIFIER_DRAW_ROUTE_IMPL
 }
 
 void SabotageSpecifier::init(SDL_Rect r)
 {
   rect = UI::Box(r);
-  SPECIFIER_HOW_SPEC
-  SPECIFIER_WHICH_SPEC
-  SPECIFIER_WHO_SPEC
-  SPECIFIER_WHEN_SPEC
+  SPECIFIER_HOW_IMPL
+  SPECIFIER_WHICH_IMPL
+  SPECIFIER_WHO_IMPL
+  SPECIFIER_WHEN_IMPL
 }
 SpecifyRequest SabotageSpecifier::touch(In &in)
 {
@@ -111,7 +163,22 @@ void SabotageSpecifier::tick()
 }
 void SabotageSpecifier::draw(Graphics *g)
 {
-
+  if(action->how == '0')
+  {
+    how.draw(g);
+    how_block.draw(g);
+    how_read.draw(g);
+    how_switch.draw(g);
+  }
+  else if(action->how == 's')
+  {
+    if(action->which == '0')
+      SPECIFIER_DRAW_WHICH_IMPL
+    else if(action->which == 'o')
+      SPECIFIER_DRAW_WHO_IMPL
+    else if(action->which == 'e')
+      SPECIFIER_DRAW_WHEN_IMPL
+  }
 }
 
 void ScoutSpecifier::init(SDL_Rect r)
@@ -135,7 +202,6 @@ void ScoutSpecifier::draw(Graphics *g)
 
 SpecifyTurnPicker::SpecifyTurnPicker()
 {
-
 }
 
 SpecifyTurnPicker::SpecifyTurnPicker(Turn *t, UI::Box b)
@@ -167,6 +233,18 @@ SpecifyTurnPicker::SpecifyTurnPicker(Turn *t, UI::Box b)
 
   cancel  = UI::TextButton("Cancel", box.x+(box.w/2)-100-50,box.y+box.h-15,100,30);
   confirm = UI::TextButton("Confirm",box.x+(box.w/2)+100-50,box.y+box.h-15,100,30);
+
+  SDL_Rect r;
+  r.x = b.x+10;
+  r.w = b.w-20;
+  r.h = b.h/2;
+  r.y = b.y+b.h-r.h-10;
+
+  attack_specifier.init(r);
+  defend_specifier.init(r);
+  message_specifier.init(r);
+  sabotage_specifier.init(r);
+  scout_specifier.init(r);
 }
 
 SpecifyTurnPicker::~SpecifyTurnPicker()
